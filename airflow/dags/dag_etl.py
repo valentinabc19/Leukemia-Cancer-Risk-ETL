0c4c9ab06e3d707aa99b3b4f91372fb2946ce9d9
valentinabc19 <valentina.bueno_col@uao.edu.co>
2025-05-11 18:19:15 -0500

    airflow dag finally working (without kafaka implementation)

--- airflow/dags/dag_etl.py
+++ airflow/dags/dag_etl.py
@@ -5,10 +5,10 @@ import pandas as pd
 import os
 import sys
 
-ROOT_DIR = os.path.abspath(os.path.join(__file__, "../"))
-
+ROOT_DIR = os.path.abspath(os.path.join(__file__, "../../"))
+print(ROOT_DIR)
 if ROOT_DIR not in sys.path:
-	sys.path.append(ROOT_DIR)      
+    sys.path.append(ROOT_DIR)
 
 from functions.leukemia_extract import extract_data
 from functions.api_extraction import api_data_extraction
@@ -28,53 +28,62 @@ default_args = {
 with DAG(
     dag_id='leukemia_etl_dag',
     default_args=default_args,
-    schedule_interval=None,
+    schedule=None,
     catchup=False
 ) as dag:
-    
-    def extract_task():
-        df=extract_data()
-        raw_path='/tmp/leukemia_raw_data.csv'
-        df.to_csv(raw_path, index=False)
-        return raw_path
-    
-    extract_operation=PythonOperator(
-        task_id='extract_leukemia_data',
-        python_callable=extract_task
-    )
 
-    def transform_task(**context):
-        """
-        Transforms data from the CSV file into dimensional tables (MedicalHistory, Region, PatientInfo)
-        and fact tables (FAct_Leukemia) using the process_dimensions() function.
-        
-        The transformed tables are saved as temporary CSV files in /tmp.
-        
-        Args:
-            context_ Airflow exexution context (used to retrieve XComs).
-        
-        Returns:
-            list: List of processed table names"""
+    def extract_leukemia_task():
+        df = extract_data()
+        path = '/tmp/leukemia_df.csv'
+        df.to_csv(path, index=False)
+        return path
 
-        raw_path=context['ti'].xcom_pull(task_ids='extract_leukemia_data')
-        df=pd.read_csv(raw_path)
-        dimensions=process_dimensions(df)
+    def extract_api_task():
+        df = api_data_extraction()
+        path = '/tmp/api_df.csv'
+        df.to_csv(path, index=False)
+        return path
 
-        for name, dim_df in dimensions.items():
+    def process_api_task(**context):
+        api_path = context['ti'].xcom_pull(task_ids='extract_api_data')
+        df = pd.read_csv(api_path)
+        processed = process_world_bank_data(df)
+        path = '/tmp/api_processed.csv'
+        processed.to_csv(path, index=False)
+        return path
+
+    def merge_task(**context):
+        leukemia_path = context['ti'].xcom_pull(task_ids='extract_leukemia_data')
+        api_path = context['ti'].xcom_pull(task_ids='process_api_data')
+        df1 = pd.read_csv(leukemia_path)
+        df2 = pd.read_csv(api_path)
+        merged = merge_dataframes(df1, df2)
+        path = '/tmp/merged_df.csv'
+        merged.to_csv(path, index=False)
+        return path
+
+    def dimension_task(**context):
+        merged_path = context['ti'].xcom_pull(task_ids='merge_data')
+        df = pd.read_csv(merged_path)
+        dim_dict = process_dimensions(df)
+
+        for name, dim_df in dim_dict.items():
             dim_df.to_csv(f'/tmp/{name}.csv', index = False)
         
-        return list(dimensions.keys())
-    
-    transform_operation=PythonOperator(
-        task_id='transform_leukemia_data',
-        python_callable=transform_task,
-        provide_context=True
-    )
+        return list(dim_dict.keys())
 
-    def load_task():
+    def validation_task():
+        dimensions_dict = {
+            'Fact_Leukemia': pd.read_csv('/tmp/Fact_Leukemia.csv'),
+            'Dim_PatientInfo': pd.read_csv('/tmp/Dim_PatientInfo.csv'),
+            'Dim_MedicalHistory': pd.read_csv('/tmp/Dim_MedicalHistory.csv'),
+            'Dim_Region': pd.read_csv('/tmp/Dim_Region.csv')
+        }
+        result = validation_results(dimensions_dict)
+        return result
 
-        creds_path="/home/ubuntu/Escritorio/Leukemia-Cancer-Risk-ETL/credentialsdb.json"
-        creds = load_db_credentials(creds_path)
+    def load_task(**context):
+        validation_success = context['ti'].xcom_pull(task_ids='validate_data')
 
         dimensions_dict = {
             'Fact_Leukemia': pd.read_csv('/tmp/Fact_Leukemia.csv'),
@@ -83,12 +92,43 @@ with DAG(
             'Dim_Region': pd.read_csv('/tmp/Dim_Region.csv')
         }
 
-        export_to_postgres(dimensions_dict, creds)
+        export_to_postgres(dimensions_dict, validation_success)
 
-    load_operation = PythonOperator(
-        task_id='load_leukemia_data',
-        python_callable=load_task,
+    extract_leukemia_op = PythonOperator(
+        task_id='extract_leukemia_data',
+        python_callable=extract_leukemia_task
     )
 
+    extract_api_op = PythonOperator(
+        task_id='extract_api_data',
+        python_callable=extract_api_task
+    )
 
-    extract_operation >> transform_operation >> load_operation
+    process_api_op = PythonOperator(
+        task_id='process_api_data',
+        python_callable=process_api_task
+    )
+
+    merge_op = PythonOperator(
+        task_id='merge_data',
+        python_callable=merge_task
+    )
+
+    transform_op = PythonOperator(
+        task_id='transform_dimensions',
+        python_callable=dimension_task
+    )
+
+    validate_op = PythonOperator(
+        task_id='validate_data',
+        python_callable=validation_task
+    )
+
+    load_op = PythonOperator(
+        task_id='load_data',
+        python_callable=load_task
+    )
+
+    extract_api_op >> process_api_op
+    [extract_leukemia_op, process_api_op] >> merge_op
+    merge_op >> transform_op >> validate_op >> load_op
